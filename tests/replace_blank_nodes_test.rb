d8c0515fe5719ecd6fc9ef9064af6ef6b2b89246
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-03-28 17:33:44 -0400

    remamed fixture

--- tests/replace_blank_nodes_test.rb
+++ tests/replace_blank_nodes_test.rb
@@ -7,22 +7,19 @@ class ReplaceBlankNodesTest < Minitest::Test
     @sparql = SPARQL.parse(File.read(sparql_file).gsub('place_url','http://my_place.com'), update: true)
   end
 
-  def test_blank_nodes_per_class
-    graph = RDF::Graph.load("./fixtures/blank_nodes_2_classes.jsonld")
+  def test_blank_nodes_per_entity
+    graph = RDF::Graph.load("./fixtures/blank_nodes_2_entities.jsonld")
     graph.query(@sparql)
-    puts "after: #{graph.dump(:jsonld)}"
+    # puts "after: #{graph.dump(:jsonld)}"
 
     expected = false
-    actual = graph.query([RDF::URI('http://my_place.com'), RDF::type, RDF::Vocab::SCHEMA.Place]).first.node?
+    actual = graph.query([RDF::URI('http://my_place.com'), RDF::type, RDF::Vocab::SCHEMA.Place])&.first&.node?
     assert_equal expected, actual, "my_place.com should not be a blank node" 
     
     expected = 1
     actual = graph.query([RDF::URI('http://my_place.com'), RDF::Vocab::SCHEMA.name, nil]).count
-    assert_equal expected, actual, "Each node should have only 1 name"
+    assert_equal expected, actual, "Different entities should not be merged"
 
-    expected = 1
-    actual = graph.query([RDF::URI('http://my_place.com'), RDF::type, RDF::Vocab::SCHEMA.Place]).count
-    assert_equal expected, actual, "my_place.com should be a schema:Place"
     
 
   end
